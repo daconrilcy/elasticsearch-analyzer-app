c65122180b64c1a72792a0c1e4b28e6000f4e240
daconrilcy <daconrilcy.com>
2025-07-27 16:28:50 +0200

    comit without cached

--- README.md
+++
@@ -1,2 +0,0 @@
-# Version recommandée pour la compatibilité Passlib/Bcrypt
-pip install bcrypt==4.0.1
--- backend/app/domain/dataset/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.domain.dataset.models import *
-from app.domain.dataset.services import *
-from app.domain.dataset.schemas import *
